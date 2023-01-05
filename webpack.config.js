6780a29b5ba07ae124248cd68b8a148a6469d478
saller <relizanmail@gmail.com>
2023-01-06 00:30:00 +0300

    add react

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,13 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: ["@babel/polyfill", "./src/index.jsx"],
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].[hash].js"
     },
     devServer: {
-        port:3000
+        port:8000
     },
     plugins: [
         new HTMLWebpackPlugin({template: "./src/index.html"}),
@@ -25,6 +25,26 @@ module.exports = {
             {
                 test: /\.(jpg|jpeg|svg)/,
                 use: ["file-loader"]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.m?jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-react", '@babel/preset-env']
+                    }
+                }
             }
         ]
     }
