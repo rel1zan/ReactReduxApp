497153c9c5903fd71fd24b3517b5522578961fd0
saller <relizanmail@gmail.com>
2023-01-09 01:31:20 +0300

    add data request and output

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 import './index.less'
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./components/App.jsx";
 import {Provider} from "react-redux";
 import { store } from "./reducers";
 
-render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
     <Provider store={store}>
         <App/>
-    </Provider>,
-    document.getElementById("root")
+    </Provider>
 )
