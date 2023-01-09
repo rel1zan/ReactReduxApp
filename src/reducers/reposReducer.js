80352accfe1a479b992e4f957f01469f96de9b33
saller <relizanmail@gmail.com>
2023-01-09 05:05:09 +0300

    add search and loading animation

--- src/reducers/reposReducer.js
+++ src/reducers/reposReducer.js
@@ -1,4 +1,5 @@
 const SET_REPOS = "SET_REPOS";
+const SET_IS_FETCHING = "SET_IS_FETCHING";
 
 const defaultState = {
     items: [],
@@ -6,16 +7,23 @@ const defaultState = {
 }
 
 
-export default function reposReducer(state = defaultState, action){
+export default function reposReducer(state = defaultState, action) {
     switch (action.type) {
         case SET_REPOS:
             return {
                 ...state,
-                items: action.payload.items
+                items: action.payload.items,
+                isFetching: false
+            }
+        case SET_IS_FETCHING:
+            return {
+                ...state,
+                isFetching: action.payload
             }
         default:
             return state
     }
 }
 
-export const setRepos = (repos) => ({type:SET_REPOS, payload:repos})
+export const setRepos = (repos) => ({type: SET_REPOS, payload: repos})
+export const setIsFetching = (bool) => ({type: SET_IS_FETCHING, payload: bool})
